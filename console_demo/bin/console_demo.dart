861912976cd60fbb5f3a81008b0ddadce3136c9b
lyj138184 雷 <138184138@qq.com>
2024-04-09 07:03:59 +0000

    初始化创建项目

---
+++ console_demo/bin/console_demo.dart
@@ -0,0 +1,5 @@
+import 'package:console_demo/console_demo.dart' as console_demo;
+
+void main(List<String> arguments) {
+  print('Hello world: ${console_demo.calculate()}!');
+}
---
+++ console_demo/test/console_demo_test.dart
@@ -0,0 +1,8 @@
+import 'package:console_demo/console_demo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
